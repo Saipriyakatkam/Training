51495564deda676089a7751f1695726d7fa8d873
Saipriyakatkam <51582050+Saipriyakatkam@users.noreply.github.com>
2020-08-27 09:39:28 +0530

    Book .java in book package

---
+++ CoreJava/BookNotFoundAsgn/com/book/Book.java
@@ -0,0 +1,70 @@
+package com.book;
+
+public class Book {
+
+	private long bIsbn;
+	private String name;
+	private double price;
+	private String publication;
+	private String author;
+	public Book()
+	{
+		
+	}
+	public Book(long bIsbn,String name,double price)
+	{
+		this.bIsbn=bIsbn;
+		this.name=name;
+		this.price=price;
+	}
+	public Book(long bIsbn,String name,double price,String publication,String author)
+	{
+
+		this.bIsbn=bIsbn;
+		this.name=name;
+		this.price=price;
+		this.publication=publication;
+		this.author=author;
+	}
+
+	
+	public long getbIsbn() {
+		return bIsbn;
+	}
+	public void setbIsbn(long bIsbn) {
+		this.bIsbn = bIsbn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getPublication() {
+		return publication;
+	}
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [bIsbn=" + bIsbn + ", name=" + name + ", price=" + price + ", publication=" + publication
+				+ ", author=" + author + "]";
+	}
+	
+	
+	
+	
+}
