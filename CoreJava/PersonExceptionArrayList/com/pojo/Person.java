e3b2fc60008257d1b2eab91e0e771b89ee71c9a4
Saipriyakatkam <51582050+Saipriyakatkam@users.noreply.github.com>
2020-08-27 13:58:07 +0530

    Create Person.java

---
+++ CoreJava/PersonExceptionArrayList/com/pojo/Person.java
@@ -0,0 +1,41 @@
+package com.pojo;
+
+public class Person {
+
+	private String name;
+	private int id;
+	public Person()
+	{
+		
+	}
+	
+	public Person( int id,String name) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", id=" + id + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		Person p=(Person)obj;
+		return this.id==p.getId() && this.getName().equals(p.getName());
+	}
+}
